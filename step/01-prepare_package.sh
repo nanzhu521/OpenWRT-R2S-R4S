f5a9736ef7ca9e142c54faf4c6798001669df18e
quintus <quzhu@MBP2020.lan>
2021-01-25 03:06:48 +0800

    Update 01-prepare_package.sh

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -59,8 +59,8 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan package/le
 #svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/luci-app-ramfree package/lean/luci-app-ramfree
 
 #wrt bw monitor
-#git clone -b master --single-branch https://github.com/brvphoenix/wrtbwmon package/new/wrtbwmon
-#git clone -b master --single-branch https://github.com/brvphoenix/luci-app-wrtbwmon package/new/luci-app-wrtbwmon
+git clone -b master --single-branch https://github.com/brvphoenix/wrtbwmon package/new/wrtbwmon
+git clone -b master --single-branch https://github.com/brvphoenix/luci-app-wrtbwmon package/new/luci-app-wrtbwmon
 
 #iputils
 svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/utils/iputils package/network/utils/iputils
